c556b1d5960fa5cd3ef651342e6a6b831f74c848
Rion <103966803+LionSuzune@users.noreply.github.com>
2022-06-09 20:18:31 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,9 +20,9 @@ global.autoread = false // auto read pesan / message
 // Other
 global.botname = "Zuikaku - MD" //your bot name
 global.ownername= "Julian" //your name
-global.myweb ="https://api.zeeoneofc.xyz" //up to you
+global.myweb ="https://github.com/LionSuzune" //up to you
 global.youtube = "https://m.youtube.com/channel/UCKTdQnHWeGgwiwz35zmduGg" //up to you
-global.github = "https://github.com/LionSuzune" //up to you
+global.github = "https://github.com/LionSuzune/Zuikaku-MD" //up to you
 global.email = "lionsuzune99@gmail.com" //up to you
 global.region = "Indonesia" //up to you
 global.ownernomer = "6285795586847" // your phone number
